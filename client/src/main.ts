9be1cd867925f8763c20726c19593f0910aa3a86
ctrlouis <lo.bertschy@gmail.com>
2023-09-18 23:52:52 +0200

    Add quasar js

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,21 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
 
-import App from './App.vue'
-import router from './router'
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
 
-const app = createApp(App)
+import App from './App.vue';
+import router from './router';
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+});
+
+app.mount('#app');
