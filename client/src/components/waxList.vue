8fa24e573464c7ea734a94c4ee3351b596e48498
ctrlouis <lo.bertschy@gmail.com>
2023-09-20 17:42:55 +0200

    Add logo when bio

--- client/src/components/waxList.vue
+++ client/src/components/waxList.vue
@@ -1,8 +1,15 @@
 <template>
     <q-list bordered separator>
         <q-item v-for="item in list" clickable v-ripple>
-            <q-item-section>{{ item.label }} - {{ item.number }}</q-item-section>
-            <q-item-section>{{ item.weight_original }} Kg</q-item-section>
+            <q-item-section>
+                {{ item.label }} - {{ item.number }}
+            </q-item-section>
+            <q-item-section>
+                {{ item.weight_original }} Kg
+            </q-item-section>
+            <q-item-section>
+                <q-icon v-if="item.bio" name="eco" size="sm" class="text-green"/>
+            </q-item-section>
         </q-item>
     </q-list>
 </template>
