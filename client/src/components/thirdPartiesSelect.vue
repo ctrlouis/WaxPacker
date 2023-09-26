da64636dbbd0685c8cead47c21fb269fda56799f
ctrlouis <lo.bertschy@gmail.com>
2023-09-26 22:00:30 +0200

    Create select component for third parties

---
+++ client/src/components/thirdPartiesSelect.vue
@@ -0,0 +1,49 @@
+<template>
+    <q-select
+        dark
+        v-model="thirdPartieSelected"
+        label="Tier"
+        :options="thirdPartiesListOptions"
+        @update:model-value="onUpdate"
+    >
+        <template v-slot:no-option>
+            <q-item>
+                <q-item-section class="text-grey">
+                Aucuns resultats
+                </q-item-section>
+            </q-item>
+        </template>
+    </q-select>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import { usePocketbaseList } from '@/composables/usePocketbaseList';
+import type { QSelectOption } from 'quasar';
+
+const props = defineProps(['modelValue']);
+
+const emit = defineEmits(['update:modelValue']);
+
+const {
+    sync: syncThirdPartiesList,
+    generateListAsOptions: generateThirdPartiesListAsOptions,
+} = usePocketbaseList('third_parties');
+
+const thirdPartieSelected = ref();
+
+function onUpdate(value: QSelectOption) {
+    emit('update:modelValue', value);
+}
+
+const thirdPartiesListOptions = generateThirdPartiesListAsOptions('name');
+
+watch(() => props.modelValue, (newValue) => {
+    thirdPartieSelected.value = newValue;
+});
+
+onMounted(async () => {
+    await syncThirdPartiesList();
+    console.log(thirdPartiesListOptions.value);
+});
+</script>
