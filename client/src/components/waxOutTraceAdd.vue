7a148ca78d63fd43104e391cd89f4742ea4c4fad
ctrlouis <lo.bertschy@gmail.com>
2023-10-08 23:07:23 +0200

    Allow trace add for wax out

--- client/src/components/waxOutTraceAdd.vue
+++ client/src/components/waxOutTraceAdd.vue
@@ -4,24 +4,26 @@
     <q-dialog v-model="show">
         <q-card dark>
             <q-card-section>
-                <div class="text-h6">Nouvelle sortie de lot</div>
+                <div class="text-h6">Nouvelle sortie</div>
             </q-card-section>
 
             <q-card-section class="q-pt-none">
-                <q-input v-model="weight" dark type="number" min="0" :max="waxOutItem.weight_left" />
-                <thirdPartiesSelect v-model="thirdPartieSelected" />
+                <q-form @submit="onCreate" @reset="close" class="flex flex-col gap-y-2">
+                    <q-select standout dark v-model="waxOutSelected" :options="waxOutOptions" label="Type de document" :disable="isDefaultWaxOutItem" :readonly="isDefaultWaxOutItem" />
+                    <q-input v-model="weight" dark type="number" min="0" :max="waxOutItem.weight_left" />
+                    <thirdPartiesSelect v-model="thirdPartieSelected" />
+                    <div class="flex justify-end">
+                        <q-btn flat label="Annuler" type="reset" color="primary" />
+                        <q-btn label="Ajouter" type="submit" color="primary" />
+                    </div>
+                </q-form>
             </q-card-section>
-
-            <q-card-actions align="right">
-                <q-btn flat label="Annuler" color="primary" @click="close" />
-                <q-btn label="Ajouter" color="primary" @click="onCreate" />
-            </q-card-actions>
         </q-card>
     </q-dialog>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import type { RecordModel } from 'pocketbase';
 import { usePocketbaseList } from '@/composables/usePocketbaseList';
 import { usePocketbaseItem } from '@/composables/usePocketbaseItem';
@@ -40,17 +42,31 @@ const {
 } = usePocketbaseItem('wax_out');
 
 const {
+    sync: syncWaxOutList,
+    generateListAsOptions: generateSelectWaxOutAsOptions,
+} = usePocketbaseList('wax_out');
+
+const {
     create: createWaxOutTrace,
 } = usePocketbaseItem('wax_out_trace');
 
 const show = ref(false);
-const thirdPartieSelected = ref<SelectRecordModel|null>();
+const thirdPartieSelected = ref<SelectRecordModel>();
+const waxOutSelected = ref<SelectRecordModel>();
 const weight = ref(0);
 
 async function open() {
     show.value = true;
-    const options = {
-        filter: "weight_left > 0",
+    if (props.waxOutItem) {
+        waxOutSelected.value = {
+            label: props.waxOutItem.number,
+            value: props.waxOutItem,
+        }
+    } else if (!props.waxOutItem) {
+        const options = {
+            filter: "weight_left > 0",
+        }
+        await syncWaxOutList(options);
     }
 }
 
@@ -59,17 +75,17 @@ function close() {
 }
 
 async function onCreate() {
-    console.log(props.waxOutItem.id);
-    
     try {
-        if (weight > props.waxOutItem.weight_left) throw new Error(`La quantité utilisé ne peut pas être supérieur à la quantité restante du lot d'entrée`);
+        if (!waxOutSelected.value) throw new Error(`Aucun lot de sortie definie`);
+        if (weight > props.waxOutItem.weight_left) throw new Error(`La quantité utilisé ne peut pas être supérieur à la quantité restante du lot de sortie`);
+        if (!thirdPartieSelected.value) throw new Error(`Aucun tier definie`);
         const createTraceData = {
-            wax_out: props.waxOutItem.id,
+            wax_out: waxOutSelected.value.value.id,
             weight: weight.value,
-            third_parties: thirdPartieSelected,
+            third_partie: thirdPartieSelected.value.value.id,
         }
         const updateWaxOutData = {
-            weight_left: props.waxOutItem.weight_left - weight.value,
+            weight_left: waxOutSelected.value.value.weight_left - weight.value,
         }
         await createWaxOutTrace(createTraceData);
         await updateWaxOutItem(updateWaxOutData, props.waxOutItem.id);
@@ -82,4 +98,14 @@ async function onCreate() {
         }
     }
 }
+
+const waxOutOptions = generateSelectWaxOutAsOptions('number');
+
+const isDefaultWaxOutItem = computed(() => {
+    let is = false;
+    if (props.waxOutItem) {
+        is = true;
+    }
+    return is;
+});
 </script>
